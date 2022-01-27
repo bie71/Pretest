3153593052059ca140ce3e837c72af4a3ace7334
Bie <bie@localhost.localdomain>
2022-01-27 12:26:42 +0700

    membuat file simulasi

---
+++ day4/simulasi.js
@@ -0,0 +1,16 @@
+let angka1, angka20, angka3, a, arr;
+
+angka20 = 20;
+angka1 = 1;
+angka3 = 3;
+
+arr = ['Santai', 'Berkualitas', 'I love coding'];
+
+while (angka1 <= angka20) {
+
+   if (arr[2] === 'I love coding' && angka3 <= 20) {
+      console.log(angka3);
+   }
+   angka3 = angka3 + 2;
+   angka1++;
+}
